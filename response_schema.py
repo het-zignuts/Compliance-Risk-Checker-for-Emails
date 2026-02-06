2d3ea9c55a2521d7c73e462d81314e16758590ac
het-zignuts <hets@zignuts.com>
2026-02-06 10:37:02 +0530

    Feature: Response schema for the LLM

--- response_schema.py
+++ response_schema.py
@@ -2,10 +2,13 @@ from pydantic import BaseModel
 from typing import List
 
 class Risk(BaseModel):
-    risk_detected: str
-    reason_for_risk: str
-    suggested_alternative_wording: List[str]
-    severity_rating: int
+    """
+    Schema for individual risk detected.
+    """
+    risk_detected: str # what risk is detected
+    reason_for_risk: str # why is it a risk?
+    suggested_alternative_wording: List[str] # what alternative words can be used in the mail for it to not classify as risky
+    severity_rating: int # severity of rsk rated from 0 to 10.
 
 class RiskAnalysisResponse(BaseModel):
-    risks: List[Risk]
+    risks: List[Risk] # Collection (list) of risks with each one being of Risk schema, returned by the LLM.
